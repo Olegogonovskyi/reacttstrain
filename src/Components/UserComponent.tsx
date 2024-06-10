01dc0a8672cc1e07219b0deec7f9c50b73592b8b
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-06-10 16:14:08 +0300

    train-asynk-rtk: made base users

--- src/Components/UserComponent.tsx
+++ src/Components/UserComponent.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react';
 import {IUserModel} from "../Models/IUserModel";
 
-const UserComponent: FC<{user: IUserModel}> = ({user}) => {
+const UserComponent: FC<{ user: IUserModel }> = ({user}) => {
+    const {name, id, email} = user
     return (
         <div>
-
+            <h1>{id}: {name} - {email}</h1>
         </div>
     );
 };
